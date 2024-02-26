670d236bef5b4d9782b3f2ecfddefb1b95ee982a
yonghun.jeong <younghun.jeong@rapeech.com>
2024-02-26 16:03:38 +0900

    feat: section2
    - InMemoryUserDetailsManager 빈 생성 추가

--- security/src/main/java/com/huning/security/config/SecurityConfig.java
+++ security/src/main/java/com/huning/security/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,33 +14,26 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-    /*
-     * 사용자 설정
-     * */
-
     http.authorizeHttpRequests((authorize) -> {
       authorize.requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
         .requestMatchers("notices", "contact").permitAll();
     }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
     return http.build();
+  }
 
-    /*
-     * 모든 요청을 거부하는 설정
-     * */
-    
-    /*http.authorizeHttpRequests((authorize) -> {
-      authorize.anyRequest().denyAll();
-    }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
-    return http.build();*/
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails admin = User.withDefaultPasswordEncoder()
+      .username("admin")
+      .password("12345")
+      .authorities("admin")
+      .build();
 
-    /*
-     * 모든 요청을 허용하는 설정
-     * */
+    UserDetails user = User.withDefaultPasswordEncoder()
+      .username("user")
+      .password("12345")
+      .authorities("read")
+      .build();
 
-    /*http.authorizeHttpRequests((authorize) -> {
-      authorize.anyRequest().permitAll();
-    }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
-    return http.build();*/
+    return new InMemoryUserDetailsManager(admin, user);
   }
 }
