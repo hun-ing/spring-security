487805bdee0671e3742fc7fd8cddbfd022159f29
yonghun.jeong <younghun.jeong@rapeech.com>
2024-03-05 08:34:55 +0900

    feat: section5
    - Custom Authentication Provider 적용

---
+++ security/src/main/java/com/huning/security/config/CustomAuthenticationProvider.java
@@ -0,0 +1,49 @@
+package com.huning.security.config;
+
+import com.huning.security.customer.entity.CustomerEntity;
+import com.huning.security.customer.repository.CustomerRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+  private final CustomerRepository customerRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String username = authentication.getName();
+    String pwd = authentication.getCredentials().toString();
+    List<CustomerEntity> customer = customerRepository.findByEmail(username);
+
+    if (customer.isEmpty()) {
+      throw new BadCredentialsException("No user registered with this details!");
+    }
+
+    if (!passwordEncoder.matches(pwd, customer.getFirst().getPwd())) {
+      throw new BadCredentialsException("Invalid password!");
+    }
+
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    authorities.add(new SimpleGrantedAuthority(customer.getFirst().getRole()));
+
+    return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+  }
+
+  @Override
+  public boolean supports(Class<?> authentication) {
+    return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+  }
+}
