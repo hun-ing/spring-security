0d59026aacdd038fdf9f8d9e7f8b265747a1fa47
yonghun.jeong <jeong.yonghun90@gmail.com>
2024-03-06 15:58:42 +0900

    feat: section6
    - csrf 설정 진행중...
    - csrf 테스트 코드 추가

--- security/src/main/java/com/huning/security/config/SecurityConfig.java
+++ security/src/main/java/com/huning/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.huning.security.config;
 
+import java.util.Arrays;
+import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,18 +17,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    http
+//      .csrf(AbstractHttpConfigurer::disable)
+      .csrf((csrf) -> csrf
+        .ignoringRequestMatchers("/contact", "/register"))
       .authorizeHttpRequests((authorize) -> {
       authorize.requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
         .requestMatchers("/notices", "/contact", "/register").permitAll();
     }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedMethods(List.of("*"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setMaxAge(3600L);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
---
+++ security/src/test/java/com/huning/security/CsrfTests.java
@@ -0,0 +1,68 @@
+package com.huning.security;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.huning.security.config.SecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@ContextConfiguration
+public class CsrfTests {
+
+  private MockMvc mockMvc;
+
+  @BeforeEach
+  public void setUp(WebApplicationContext applicationContext) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+      .apply(springSecurity())
+      .build();
+  }
+
+  @Test
+  public void loginWhenValidCsrfTokenThenSuccess() throws Exception {
+    this.mockMvc.perform(post("/login").with(csrf())
+        .accept(MediaType.TEXT_HTML)
+        .param("username", "test")
+        .param("password", "test"))
+      .andDo(print())
+      .andExpect(status().is3xxRedirection())
+      .andExpect(header().string(HttpHeaders.LOCATION, "/login?error"));
+  }
+
+  @Test
+  public void loginWhenValidCsrfTokenThenFail() throws Exception {
+    this.mockMvc.perform(post("/login").with(csrf().useInvalidToken())
+        .accept(MediaType.TEXT_HTML)
+        .param("username", "test")
+        .param("password", "test"))
+      .andDo(print())
+      .andExpect(status().isForbidden())
+      ;
+  }
+
+  @Test
+  @WithMockUser
+  public void logoutWhenValidCsrfTokenThenSuccess() throws Exception {
+    this.mockMvc.perform(post("/logout").with(csrf())
+        .accept(MediaType.TEXT_HTML))
+      .andExpect(status().is3xxRedirection())
+      .andExpect(header().string(HttpHeaders.LOCATION, "/login?logout"));
+  }
+}
