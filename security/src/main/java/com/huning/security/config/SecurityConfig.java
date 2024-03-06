ba94c7a2c19b7cab4cae66b74f72060f42453964
yonghun.jeong <jeong.yonghun90@gmail.com>
2024-03-06 20:03:23 +0900

    feat: section6
    - SPA csrf 설정 추가 및 테스트 코드 추가

--- security/src/main/java/com/huning/security/config/SecurityConfig.java
+++ security/src/main/java/com/huning/security/config/SecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,12 +30,17 @@ public class SecurityConfig {
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http
 //      .csrf(AbstractHttpConfigurer::disable)
-      .csrf((csrf) -> csrf
-        .ignoringRequestMatchers("/contact", "/register"))
-      .authorizeHttpRequests((authorize) -> {
-      authorize.requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
-        .requestMatchers("/notices", "/contact", "/register").permitAll();
-    }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+        .csrf((csrf) -> csrf
+            .ignoringRequestMatchers("/contact", "/register")
+
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+        )
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .authorizeHttpRequests((authorize) -> {
+          authorize.requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
+              .requestMatchers("/notices", "/contact", "/register").permitAll();
+        }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
     return http.build();
   }
 
--- security/src/test/java/com/huning/security/CsrfTests.java
+++ security/src/test/java/com/huning/security/CsrfTests.java
@@ -2,12 +2,14 @@ package com.huning.security;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.huning.security.config.SecurityConfig;
+import java.util.Objects;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +21,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -33,6 +36,24 @@ public class CsrfTests {
     this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
       .apply(springSecurity())
       .build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    MvcResult mvcResult1 = this.mockMvc.perform(get("/"))
+        .andDo(print())
+        .andReturn();
+
+    String CSRF_VALUE = Objects.requireNonNull(mvcResult1.getResponse().getCookie("XSRF-TOKEN")).getValue();
+
+    this.mockMvc.perform(post("/login")
+            .accept(MediaType.TEXT_HTML)
+            .header("X-XSRF-TOKEN", CSRF_VALUE)
+            .param("username", "test")
+            .param("password", "test"))
+        .andDo(print())
+        .andExpect(status().is3xxRedirection())
+        .andExpect(header().string(HttpHeaders.LOCATION, "/login?error"));
   }
 
   @Test
